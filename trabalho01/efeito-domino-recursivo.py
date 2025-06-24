96b6b98333834ee9b9b0a3da2ae2fdbd5f77eaad
Gabriel Libardi <gabriel_libardi@hotmail.com>
2025-06-24 08:53:31 -0300

    Efeito dominó recursivo sem mudança de lista

--- trabalho01/efeito-domino-recursivo.py
+++ trabalho01/efeito-domino-recursivo.py
@@ -1,29 +1,30 @@
 import sys
 sys.setrecursionlimit(10000)
 
-def mov_min(linha: list[int], n: int, h: int) -> int:
+def mov_min(linha: list[int], n: int, h: int, sobra: int) -> int:
+    print(n, sobra)
     if n == 2:
-        ans = 0 if linha[0] <= h else -1
+        ans = 0 if linha[0] + sobra <= h else -1
     else:
-        if linha [n-2] < h:
-            resto = linha[n-2] - h
-            sem_resto = mov_min(linha[:n-2], n - 1, h)
-            com_resto = mov_min(linha[:n-3] + [linha[n - 3] + resto], n - 1, h)
-            if sem_resto == -1 and com_resto == -1:
+        if linha [n-2] + sobra < h:
+            resto = linha[n-2] + sobra - h
+            sem_sobra = mov_min(linha, n - 1, h, 0)
+            com_sobra = mov_min(linha, n - 1, h, resto)
+            if sem_sobra == -1 and com_sobra == -1:
                 ans = -1
-            elif sem_resto == com_resto:
-                ans = sem_resto
+            elif sem_sobra == com_sobra:
+                ans = sem_sobra
             else:
-                ans = 1 + com_resto
-        elif linha[n-2] > h:
-            resto = linha[n-2] - h
-            com_resto = mov_min(linha[:n-3] + [linha[n - 3] + resto], n - 1, h)
-            if com_resto == -1:
+                ans = 1 + com_sobra
+        elif linha[n-2] + sobra > h:
+            resto = linha[n-2] + sobra - h
+            com_sobra = mov_min(linha, n - 1, h, resto)
+            if com_sobra == -1:
                 ans = -1
             else:
-                ans = 1 + com_resto
+                ans = 1 + com_sobra
         else:
-            ans = mov_min(linha[:n-2], n - 1, h)
+            ans = mov_min(linha, n - 1, h, 0)
     return ans
 
 def main():
@@ -31,7 +32,7 @@ def main():
         n, h = list(map(int, input().strip().split()))
         if n != -1:
             linha = list(map(int, input().strip().split()))
-            print(mov_min(linha, n, h))
+            print(mov_min(linha, n, h, 0))
         else:
             break
 
